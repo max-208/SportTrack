cfb2d833b399af2d33f7933802926f9549722e84
WiPhil <philo102002@gmail.com>
2021-09-27 11:03:00 +0200

    Vue ajout d'activité

--- views/UploadActivityForm.php
+++ views/UploadActivityForm.php
@@ -7,16 +7,22 @@
 <body>
 
 <h1>SpotTrack</h1>
-<a href="index.php?page=/">upload_activity</a>
+<a href="index.php?page=/">Acceuil</a>
 <p>Ajouter une nouvelle activité</p>
 
 <form action = "index.php?page=upload_activity" method="POST">
     
-    <label for="avatar">Choisir un fichier à ajouter (format Json):</label>
+    <label for="avatar">Choisir un fichier à ajouter (format Json):
+    <br>
+    <br>
+    </label>
     
     <input type="file" id="avatar" name="avatar"
        accept=".json">
-
+    <br>
+    <br>
+    <br>
+    
     <button>Envoyer</button>
     
     
