18752167e3d48f24ee6c0a3f30bd274165f55df3
WiPhil <philo102002@gmail.com>
2021-09-27 16:24:25 +0200

    ListActivityView

--- views/ListActivityView.php
+++ views/ListActivityView.php
@@ -11,7 +11,41 @@
 <p>Liste des activités</p>
 
 <form action = "index.php?page=list_activities" method="POST">
-    
+    <<table border="1", cellpadding="8">>
+      <colgroup span="8" class="columns"></colgroup>
+      <tr>
+        <th><td width="15%">Description</th>
+        <th>Date</th>
+        <th>Heure de début</th>
+        <th>Durée</th>
+        <th>Distance parcourue</th>
+        <th>Frequence minimum</th>
+        <th>Frequence maximum</th>
+        <th>Frequence moyenne</th>
+        
+      </tr>
+      <tr>
+        <td><td width="15%">  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+
+      </tr>
+      <tr>
+        <td><td width="15%">  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+        <td>  </td>
+      </tr>
+    </table>
     
     
     
