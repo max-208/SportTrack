6c26a456653ef38c253aefa45c4d12a7b3f07b2e
WiPhil <philo102002@gmail.com>
2021-09-27 15:59:02 +0200

    Test

---
+++ views/ListActivityView.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<meta charset="UTF-8">
+<head>
+<title>SpotTrack</title>
+</head>
+<body>
+
+<h1>SpotTrack</h1>
+<a href="index.php?page=/">Acceuil</a>
+<p>Liste des activités</p>
+
+<form action = "index.php?page=list_activities" method="POST">
+    
+    
+    
+    
+    
+</form>
+</body>
+</html> 
