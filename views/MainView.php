c1b3126eb0cdcab262aaab8594e5b8f7e8cbf3e0
Maxime DANIEL <maxime.daniel208@gmail.com>
2021-10-03 19:08:21 +0200

    typo

--- views/MainView.php
+++ views/MainView.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="left-text col-lg-6 col-md-12 col-sm-12 col-xs-12">
                 <h1>Le sport ne forge pas le caractère.</br><em>Il le révèle.</em></h1>
-                <p>Sport Track est appecation qu'il vous faut. Celle-ci vous offre une base intéressante pour suivre les efforts physiques qu'ils agisse de marche, de course, de randonnée de vélo, ou de sports intérieur ou extérieur, il est possible d'ajouter et suivre un grand nombre d'activités sportives à l'aide des différents dispositifs de suivi intégrés. Cet outil est une aide précieuse pour analyser vos différentes activités sportives au cours du temps</p> 
+                <p>Sport Track est l'application qu'il vous faut. Celle-ci vous offre une base intéressante pour suivre les efforts physiques qu'il s'agisse de marche, de course, de randonnée de vélo, ou de sports intérieur ou extérieur, il est possible d'ajouter et suivre un grand nombre d'activités sportives à l'aide des différents dispositifs de suivi intégrés. Cet outil est une aide précieuse pour analyser vos différentes activités sportives au cours du temps</p> 
                 <a href="index.php?page=user_add_form" class="main-button-slider">REJOINDRE SPORTTRACK</a>
             </div>
         </div>
